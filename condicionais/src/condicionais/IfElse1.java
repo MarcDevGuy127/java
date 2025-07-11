52b45978f1181760a07e4b8e55f79ebfe3c647fe
MarcDevGuy127 <marceloh.github@gmail.com>
2025-07-11 18:46:20 -0300

    Saving

--- condicionais/src/condicionais/IfElse1.java
+++ condicionais/src/condicionais/IfElse1.java
@@ -5,23 +5,17 @@ public class IfElse1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x = 10;
-		int y = 9;
 		
-		if (x < 10 && !(y > 10)) {
-			System.out.println(x + " é menor que 10");
-			System.out.println(y + " não é maior que 10");
+		if (!(x <= 11)) {
+			System.out.println("x é maior que 11");
 		} else {
 			if (x == 10) {
-				System.out.println(x + " é igual 10");
+				System.out.println("x é igual a 10");
 			} else {
-				if (y == 20 || x == 10) {
-					System.out.println(y + " está entre 10 e 20");
-				} else {
-					System.out.println(x + " vale 10");
-				}
+				System.out.println("x não é 10");
 			}
-
 		}
+		
 	}
 
 }
---
+++ condicionais/src/condicionais/IfElse2.java
@@ -0,0 +1,16 @@
+package condicionais;
+
+public class IfElse2 {
+
+	public static void main(String[] args) {
+		
+		int num = 10;
+		
+		if (num == 10) {
+			System.out.println("num vale 10");
+		} else {
+			System.out.println("num não vale 10");
+		}
+	}
+
+}
