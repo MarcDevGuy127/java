df0b4620dbe971ca5c58b4bd69c7bf81f9f34790
MarcDevGuy127 <marceloh.github@gmail.com>
2025-07-13 16:24:33 -0300

    Add problem example resolution #1

--- condicionais/src/condicionais/Exemplo1.java
+++ condicionais/src/condicionais/Exemplo1.java
@@ -8,27 +8,33 @@ public class Exemplo1 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		int adicional;
-		int mult;
+		int restante = 0;
+		int adicional = 0;
 		int valor = 50;
-		System.out.println("Digite");
+		
+		System.out.println("Digite a quantidade de minutos:"); //interação do usuário
 		int minutos = sc.nextInt();
+		
+		
+		if (minutos < 100) System.out.println("Mínimo 100 minutos");
 		
 		if (minutos == 100) {
 			
-			System.out.println("Pague " + valor);
-			
+			System.out.println("Minutos: " + minutos);
+			System.out.println("Pague: " + valor);
 		} 
 		
-		if (minutos >= 101){
+		if (minutos > 100){
 
-			adicional = 100 - minutos;
-				
-			mult = adicional * 2; // a diferenca vale a multiplicacao com 2
+			//subtração da qntde de mins com o valor minimo de 100 mins
+			restante = minutos - 100;
 			
-			valor = mult + valor;
-				
-			System.out.println("Pague " + valor);
+			adicional = restante * 2; //a diferenca vale a multiplicacao com 2
+			valor = valor + adicional;
+			
+			System.out.println("Minutos: " + minutos);
+			System.out.println("Adicional: " + adicional);
+			System.out.println("Pague: " + valor);
 		}
 	}
 
