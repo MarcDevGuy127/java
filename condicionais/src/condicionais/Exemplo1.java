7ef861eefc83dd7783f7a8f88f7c3746f98867d8
MarcDevGuy127 <marceloh.github@gmail.com>
2025-07-13 16:53:43 -0300

    Add problem example resolution #2

--- condicionais/src/condicionais/Exemplo1.java
+++ condicionais/src/condicionais/Exemplo1.java
@@ -5,37 +5,30 @@ import java.util.Scanner;
 public class Exemplo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Scanner sc = new Scanner(System.in);
 		
-		int restante = 0;
-		int adicional = 0;
+		int diferenca, adicional = 0;
 		int valor = 50;
 		
 		System.out.println("Digite a quantidade de minutos:"); //interação do usuário
 		int minutos = sc.nextInt();
 		
+		if (minutos < 0) System.out.println("Valor inválido!");
 		
 		if (minutos < 100) System.out.println("Mínimo 100 minutos");
 		
-		if (minutos == 100) {
-			
-			System.out.println("Minutos: " + minutos);
-			System.out.println("Pague: " + valor);
-		} 
+		if (minutos == 100) System.out.println("Valor a pagar: R$ " + valor);
 		
-		if (minutos > 100){
+		if (minutos > 100) {
 
-			//subtração da qntde de mins com o valor minimo de 100 mins
-			restante = minutos - 100;
-			
-			adicional = restante * 2; //a diferenca vale a multiplicacao com 2
+			diferenca = minutos - 100; //subtração da qntde de mins com o minimo de 100 mins
+			adicional = diferenca * 2; //a diferenca vale a multiplicacao com 2
 			valor = valor + adicional;
 			
-			System.out.println("Minutos: " + minutos);
-			System.out.println("Adicional: " + adicional);
-			System.out.println("Pague: " + valor);
+			System.out.println("Valor a pagar: R$ " + valor);
 		}
+		sc.close();
 	}
 
 }
