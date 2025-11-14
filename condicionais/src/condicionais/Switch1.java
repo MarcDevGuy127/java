38a393113b3638abb8ed5f949eca59ffaf71c61a
MarcDevGuy127 <marceloh.github@gmail.com>
2025-11-14 15:28:25 -0300

    Refactoring switch case and implementing while loop.

--- condicionais/src/condicionais/Switch1.java
+++ condicionais/src/condicionais/Switch1.java
@@ -7,37 +7,28 @@ public class Switch1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		int diaSemana = 0;
 		
-		System.out.println("Digite um valor(1 a 7):");
-		int diaSemana = sc.nextInt();
-		
-		switch (diaSemana) {
-		case 1:
-			System.out.println("Segunda-feira");
-			break;
-		case 2:
-			System.out.println("Terça-feira");
-			break;
-		case 3:
-			System.out.println("Quarta-feira");
-			break;
-		case 4:
-			System.out.println("Quinta-feira");
-			break;
-		case 5:
-			System.out.println("Sexta-feira");
-			break;
-		case 6:
-			System.out.println("Sábado");
-			break;
-		case 7:
-			System.out.println("Domingo");
-			break;
-		default:
-			if (diaSemana <= 0 || diaSemana > 7) {
-				System.out.println("Valor inválido!");
+		while(diaSemana != 8) {
+			System.out.println("Digite um valor(1 a 7):");
+			System.out.println("8 - Sair");
+			
+			diaSemana = sc.nextInt();
+			
+			switch (diaSemana) {
+				case 1 -> System.out.println("Segunda-feira");
+				case 2 -> System.out.println("Terça-feira");
+				case 3 -> System.out.println("Quarta-feira");
+				case 4 -> System.out.println("Quinta-feira");
+				case 5 -> System.out.println("Sexta-feira");
+				case 6 -> System.out.println("Sábado");
+				case 7 -> System.out.println("Domingo");
+				default -> {
+					if (diaSemana <= 0 || diaSemana > 8) {
+						System.out.println("Valor inválido!");
+					}
+				}
 			}
-			break;
 		}
 	}
 
