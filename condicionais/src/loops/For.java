7294a9590396e4bd78896e1a18a75114f3fce54a
MarcDevGuy127 <marceloh.github@gmail.com>
2025-11-30 16:56:39 -0300

    Add for lesson.

---
+++ condicionais/src/loops/For.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class For {
+
+	public static void main(String[] args) {
+		int x = 10;
+		
+		for (int i = 1; i <= x; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Acabou a contagem!");
+	}
+
+}
