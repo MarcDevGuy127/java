017869edaf9245469713c5f0843a701388b4c10a
MarcDevGuy127 <marceloh.github@gmail.com>
2025-12-05 14:17:24 -0300

    Add decrement for example.

--- condicionais/src/loops/For_Execucao.java
+++ condicionais/src/loops/For_Execucao.java
@@ -3,9 +3,16 @@ package loops;
 public class For_Execucao {
 
 	public static void main(String[] args) {
-		// Teste de execução
+		// Teste de execução com incrementacao
 		for (int i = 0; i < 5; i++) {
 			System.out.printf("Valor de i: %d \n", i);
+		}
+		
+		System.out.println("\n");
+		
+		// Teste de execução com incrementacao
+		for (int j = 5; j >= 0; j--) {
+			System.out.printf("Valor de j: %d \n", j);
 		}
 	}
 
