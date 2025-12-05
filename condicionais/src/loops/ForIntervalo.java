3a4b19bbc0d45f2679e0ac86090c257eb0351018
MarcDevGuy127 <marceloh.github@gmail.com>
2025-12-05 15:30:40 -0300

    Add exercises4 for loop exercise 2.

---
+++ condicionais/src/loops/ForIntervalo.java
@@ -0,0 +1,34 @@
+package loops;
+
+import java.util.Scanner;
+
+public class ForIntervalo {
+    public static void main(String[] args) {
+        /*
+        Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X 
+        que serão lidos em seguida. Mostre quantos destes valores X estão dentro do 
+        intervalo [10,20] e quantos estão fora do intervalo, mostrando essas informações 
+        conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para 
+        fora do intervalo).
+        */
+        Scanner sc = new Scanner(System.in);
+        
+        int inCount = 0, outCount = 0;
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            int x = sc.nextInt();
+            
+            if (x >= 10 && x <= 20) {
+                inCount++;
+            } else {
+                outCount++;
+            }
+        }
+        
+        System.out.printf("%d in\n", inCount);
+        System.out.printf("%d out\n", outCount);
+        
+        sc.close();
+    }
+}
