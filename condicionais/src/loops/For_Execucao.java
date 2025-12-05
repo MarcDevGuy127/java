60dc3f835c073c947ec6c30be71d677e534e3020
MarcDevGuy127 <marceloh.github@gmail.com>
2025-12-05 14:09:01 -0300

    Add for loop running example.

---
+++ condicionais/src/loops/For_Execucao.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class For_Execucao {
+
+	public static void main(String[] args) {
+		// Teste de execução
+		for (int i = 0; i < 5; i++) {
+			System.out.printf("Valor de i: %d \n", i);
+		}
+	}
+
+}
