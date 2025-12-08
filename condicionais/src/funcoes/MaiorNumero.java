9c30fb5ea90175103315790ab6dbeb8e3413bf4f
MarcDevGuy127 <marceloh.github@gmail.com>
2025-12-08 20:29:18 -0300

    Add function to high number comparative lesson.

--- condicionais/src/funcoes/MaiorNumero.java
+++ condicionais/src/funcoes/MaiorNumero.java
@@ -10,13 +10,24 @@ public class MaiorNumero {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
-		if (a > b && a > c) {
-			System.out.println("Higher = " + a);
-		} else if (b > c) {
-			System.out.println("Higher = " + b);
-		} else {
-			System.out.println("Highe r= " + c);
-		}
+		int higher = max(a, b, c);
+		showResult(higher);
 		sc.close();
+	}
+
+	public static int max(int x, int y, int z) {
+		int aux;
+		if (x > y && x > z) {
+			aux = x;
+		} else if (y > z) {
+			aux = y;
+		} else {
+			aux = z;
+		}
+		return aux;
+	}
+
+	public static void showResult(int value) {
+		System.out.println("Higher = " + value);
 	}
 }
